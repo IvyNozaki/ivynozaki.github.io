6481406320d63ad26a8c0dbb7435042c02710710
Ivy Nozaki <ivyacasio@gmail.com>
2021-04-27 13:15:41 -1000

    updated navigation for both pages

---
+++ README.md
@@ -0,0 +1,30 @@
+# Personal Portfolio
+
+---
+
+## **Description**
+
+A personal portfolio that shows my bio, how to connect with me and my current projects.
+
+---
+
+## **How to run**
+
+My main page opens up in a night cloudy sky, which clears up as you scroll down. The dark moon also brightens up revealing a self photo. The moon moves down the page as you scroll and stops just above where my bio and youtube videos are.
+
+You can hover over the icons to reveal my contact info.
+
+Below the bio section, there are balloons carrying an image of my projects. You can click on the "VIEW MY PROJECTS" to go straight to the projects page or click on the individual images to go straight to that portion of the projects page.
+
+## **Road Map**
+
+1. Make on scroll animations smoother for mouse use. It's smooth on trackpads but not the mouse.
+2.
+
+---
+
+## **Licenses**
+
+- MIT
+- Images created by using Affinity Designer.
+- Custom styling and code using HTML, CSS and JavaScript.
--- projects.js
+++ projects.js
@@ -13,8 +13,30 @@ function justSmile() {
     avatarGirl.style.display = 'none';
 }
 
-function speak() {
-    
+////////////////////////
+// NAV BAR FUNCTIONS //
+////////////////////////
+var navCloud = document.getElementById('contact-section');
+var contactIcons = document.getElementsByClassName('contact-info')
+
+function showCloud () {
+  navCloud.style.width = '200px';
+  navCloud.style.height = '500px';
+  navCloud.style.right = '0px';
+  navCloud.style.backgroundImage = 'url("assets/nav-thought-1.png")';
+  navCloud.style.color = 'black';
+  contactIcons[0].style.display = "block";
+  document.getElementsByClassName("exit")[0].style.display = "block";
+}
+
+function hideCloud () {
+  navCloud.style.width = "160px";
+  navCloud.style.height = "150px";
+  navCloud.style.backgroundImage = 'url("assets/thought.png")';
+  navCloud.style.color = 'transparent';
+  contactIcons[0].style.display = "none";
+  console.log('hide cloud');
+  document.getElementsByClassName("exit")[0].style.display = "none";
 }
 
 ////////////////////////
